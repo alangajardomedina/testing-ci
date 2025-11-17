616fdbaca1d6d32749119b5bc99496d11e40b256
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2025-11-17 15:17:05 -0300

    actualizacion - se crea prueba inicial

--- test/placeholder.test.js
+++ test/placeholder.test.js
@@ -1,3 +1,8 @@
-test('placeholder', ()=>{
-    expect(true).toBe(true)
+const request = require('supertest')
+const app = require('../src/index')
+
+test('GET / debe responder con mensaje correcto', async ()=>{
+    const response = await request(app).get('/')
+    expect(response.statusCode).toBe(200)
+    expect(response.text).toBe('Servidor levantado correctamente')
 })
