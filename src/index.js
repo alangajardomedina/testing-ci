fdad78ddfcf583228a8d661e4e942bcf5e8df5db
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2025-11-17 11:27:48 -0300

    primer commit - subida y testing

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res) => {
+    res.send('Servidor levantado correctamente')
+})
+
+module.exports = app
+
+if(require.main===module){
+    app.listen(3000, ()=>{
+        console.log('Servidor corriendo en http://locahost:3000')
+    })
+}
