cbe1a640a69a63371d1604d62c5d8d4671b25473
alangajardomedina <alan.gajardo@profesor.duoc.cl>
2025-11-17 15:19:13 -0300

    actualización - mensaje erroneo actualizado de la api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-    res.send('Servidor levantado correctamente')
+    res.send('Servidor levantado mal')
 })
 
 module.exports = app
